87b5df0dbd4431a562e477bf26d255b609e05548
fuzhongqi <2858199552@qq.com>
2019-11-26 23:34:26 +0800

    one AC project

--- tt.c
+++ tt.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+int x = 0;
+int main()
+{
+  printf("hello :\n%d\n", ++x);
+  printf("you can change next perpor's [ x ]!!\n");
+  while(scanf("%d", &x) != EOF){
+    if(x < 0){
+      return 0;
+    }
+  }
+  return 0;
+}
